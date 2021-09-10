136579ac3671b2384c9124511d771aa9d0b750f9
Felipe Garcia Fogal <felipefogal@gmail.com>
2021-09-10 18:44:32 -0300

    Alterando para trabalhar com persons e criando model Person

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+    }
+}

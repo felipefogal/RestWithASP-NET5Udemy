20e03c230e35214ad39baafcd575d0c27939a0b9
Felipe Garcia Fogal <felipefogal@gmail.com>
2021-09-13 23:02:42 -0300

    Concluindo persistências do banco de dados e suas conexões

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestWithASPNETUdemy.Models.Context;
 using RestWithASPNETUdemy.Services;
 using RestWithASPNETUdemy.Services.Implementations;
 
@@ -30,6 +25,13 @@ namespace RestWithASPNETUdemy
         {
 
             services.AddControllers();
+
+            // ATENÇÃO: Estas configurações mudam nas últimas versões do EntityFramework
+            var connection = Configuration["ConnectionStrings:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => {
+                options.UseMySql(connection);
+            });
+
             services.AddScoped<IPersonService, PersonServiceImplementation>();
 
             services.AddSwaggerGen(c =>
